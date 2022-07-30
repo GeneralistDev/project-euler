f62d8f137a1e15d99e760c7581890dc6bebfeda9
Daniel Parker <daniel@danielparker.com.au>
2022-07-30 12:39:36 -0500

    Problem 2

--- problems/problem2/src/main.rs
+++ problems/problem2/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a = 1;
+    let mut b = 2;
+    let mut c = a + b;
+
+    let mut sum_even = 2;
+
+    while c < 4_000_000 {
+        if c % 2 == 0 {
+            sum_even = sum_even + c;
+        }
+        a = b;
+        b = c;
+        c = a + b;
+    }
+
+    println!("Sum: {}", sum_even);
 }
